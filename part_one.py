0bf25615b652d0b230f6d55f4ac4680968e82d1c
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-06-14 23:07:20 +0300

    find_eigenvalues_eigenvectors()
    -created func to find eigenvalues&eigenvectors of matrix

--- part_one.py
+++ part_one.py
@@ -1,5 +1,16 @@
+import numpy as np
+
+
 def is_square_matrix(matrix):
     if matrix.shape[0] != matrix.shape[1]:
         print("Matrix is not square")
         return False
     return True
+
+
+def find_eigenvalues_eigenvectors(matrix):
+    if not is_square_matrix(matrix):
+        return
+
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+    return eigenvalues, eigenvectors
