263aefe7e3914166a6dea20abf1dd42d53181b31
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-06-17 01:23:29 +0300

    modified arbitrary_matrix
    -replaced separate variable n for dimension with lambda n

--- objects.py
+++ objects.py
@@ -1,8 +1,8 @@
 import numpy as np
 import random
 
-n = random.randint(1, 10)
-arbitrary_matrix = np.random.randint(0, 256, (n, n))
+
+arbitrary_matrix = np.random.randint(0, 256, (lambda n=random.randint(1, 5): (n, n))())
 
 image = 'image.png'
 
