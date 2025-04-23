ea3bcfaf0f506ce7637ff77db6e23c7968e8fbff
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-04-23 22:28:54 +0300

    update project structure

--- objects.py
+++ objects.py
@@ -4,7 +4,7 @@ import random
 
 arbitrary_matrix = np.random.randint(0, 256, (lambda n=random.randint(1, 5): (n, n))())
 
-image = 'image.png'
+image = 'assets/image.png'
 
 message = 'чіназес!'
 key_matrix = np.random.randint(0, 256, (len(message), len(message)))
